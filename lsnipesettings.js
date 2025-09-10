0e2e69c16854637d5ecd652f400fe5daaf3a8f1b
audaceetpanache <230256931+audaceetpanache@users.noreply.github.com>
2025-09-10 18:25:09 +0200

    Update lsnipesettings.js

--- lsnipesettings.js
+++ lsnipesettings.js
@@ -1,9 +1,9 @@
 export function getLsniperSettingsMenu() {
   return {
     inline_keyboard: [
-      [{ text: "⚡ Auto-snipe ON/OFF", callback_data: "toggle_autosnipe" }],
-      [{ text: "💰 Budget", callback_data: "set_budget" }],
-      [{ text: "⏱ Délai", callback_data: "set_delay" }],
+      [{ text: "⚡ Auto-snipe ON/OFF", callback_data: "ls_unavailable" }],
+      [{ text: "💰 Budget", callback_data: "ls_unavailable" }],
+      [{ text: "⏱ Délai", callback_data: "ls_unavailable" }],
       [{ text: "⬅️ Retour", callback_data: "home" }],
     ],
   };
