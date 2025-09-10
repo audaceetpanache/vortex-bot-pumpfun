2d39c54865b85cb64e8c154c70971e0db9ced657
audaceetpanache <230256931+audaceetpanache@users.noreply.github.com>
2025-09-10 17:51:12 +0200

    Update projectStore.js

--- projectStore.js
+++ projectStore.js
@@ -1,55 +1,33 @@
-// projectStore.js
-class ProjectStore {
-  constructor() {
-    this.projects = {}; // { chatId: [ {id, name, symbol, description, wallet} ] }
-    this.editing = {};  // { chatId: { projectId, field } }
-  }
+export const projectStore = {
+  projects: {},
 
   getProjects(chatId) {
-    return this.projects[chatId] || [];
-  }
-
-  addProject(chatId, name) {
-    if (!this.projects[chatId]) this.projects[chatId] = [];
-    const newProj = {
-      id: Date.now().toString(),
-      name,
-      symbol: "",
-      description: "",
-      wallet: ""
-    };
-    this.projects[chatId].push(newProj);
-    return newProj;
-  }
-
-  deleteProject(chatId, projectId) {
-    if (!this.projects[chatId]) return;
-    this.projects[chatId] = this.projects[chatId].filter(p => p.id !== projectId);
-  }
-
-  getProject(chatId, projectId) {
-    return (this.projects[chatId] || []).find(p => p.id === projectId);
-  }
-
-  setEditing(chatId, projectId, field) {
-    this.editing[chatId] = { projectId, field };
-  }
-
-  getEditing(chatId) {
-    return this.editing[chatId];
-  }
-
-  clearEditing(chatId) {
-    delete this.editing[chatId];
-  }
-
-  updateProjectField(chatId, projectId, field, value) {
-    const proj = this.getProject(chatId, projectId);
-    if (proj && field in proj) {
-      proj[field] = value;
+    if (!this.projects[chatId]) {
+      this.projects[chatId] = [];
     }
-    return proj;
-  }
-}
+    return this.projects[chatId];
+  },
 
-export const projectStore = new ProjectStore();
+  addProject(chatId, project) {
+    if (!this.projects[chatId]) {
+      this.projects[chatId] = [];
+    }
+    this.projects[chatId].push(project);
+    return project;
+  },
+
+  deleteProject(chatId, index) {
+    if (this.projects[chatId]) {
+      this.projects[chatId].splice(index, 1);
+    }
+  },
+
+  updateProject(chatId, index, newData) {
+    if (this.projects[chatId] && this.projects[chatId][index]) {
+      this.projects[chatId][index] = {
+        ...this.projects[chatId][index],
+        ...newData,
+      };
+    }
+  },
+};
