8116849975ac8ae4bfbd2f6cfc235b9a65e2c831
audaceetpanache <230256931+audaceetpanache@users.noreply.github.com>
2025-09-10 21:24:13 +0200

    Update unavailable.js

--- unavailable.js
+++ unavailable.js
@@ -1,9 +1,8 @@
-// unavailable.js
 export function getUnavailableMenu() {
   return {
-    text: "🚧 Cette fonctionnalité n'est pas encore disponible.",
+    text: "🚧 Cette fonctionnalité n'est pas encore disponible",
     reply_markup: {
-      inline_keyboard: [[{ text: "⬅️ Retour", callback_data: "home" }]],
-    },
+      inline_keyboard: [[{ text: "⬅️ Back", callback_data: "home" }]]
+    }
   };
 }
