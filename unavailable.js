30c27a456f0ccec6bbf263f622138547a857c0f7
audaceetpanache <230256931+audaceetpanache@users.noreply.github.com>
2025-09-10 15:07:33 +0200

    Delete unavailable.js

--- unavailable.js
+++
@@ -1,8 +0,0 @@
-export function getUnavailableMenu() {
-  return {
-    text: "🚧 This feature is not supported yet, working on it",
-    reply_markup: {
-      inline_keyboard: [[{ text: "⬅️ Back", callback_data: "home" }]]
-    }
-  };
-}
