f4c35bcfb1dc34635ce1e4f9a24918bdd050516c
audaceetpanache <230256931+audaceetpanache@users.noreply.github.com>
2025-09-19 00:04:25 +0200

    Delete unavailable.js

--- unavailable.js
+++
@@ -1,8 +0,0 @@
-export function getUnavailableMenu() {
-  return {
-    text: "🚧 Cette fonctionnalité n'est pas encore disponible",
-    reply_markup: {
-      inline_keyboard: [[{ text: "⬅️ Back", callback_data: "home" }]]
-    }
-  };
-}
